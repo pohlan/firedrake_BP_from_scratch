fefe209a2d470a3b3bbe088f6fc4d0c342de550e
Ed Bueler <elbueler@alaska.edu>
2025-07-17 14:49:33 -0600

    version with tauc turned off, but a free surface; seems to converge properly

--- schoof_2D.py
+++ schoof_2D.py
@@ -15,7 +15,7 @@ x, y = df.SpatialCoordinate(mesh)
 V = df.FunctionSpace(mesh, 'CG', 1)
 v = df.Function(V)
 f = df.Function(V).interpolate(f0 - x/2)
-tau_c = df.Function(V).interpolate(df.min_value(df.ge(abs(x),0.5), df.le(abs(x),3/2)) * tau0)
+###tau_c = df.Function(V).interpolate(df.min_value(df.ge(abs(x),0.5), df.le(abs(x),3/2)) * tau0)  FIXME: put it back
 
 # 3D
 # s = df.sqrt( u.dx(0)**2 +  u.dx(0)*v.dx(1) + v.dx(1)**2 \
@@ -25,13 +25,30 @@ s  = df.sqrt(v.dx(0)**2 + 0.25*v.dx(1)**2 + 1e-15)
 G  = 2*B*s**p / p
 
 # Coulomb
-j = tau_c * abs(v) * df.ds(1)    # id 1 : bed of gllacier
+###j = tau_c * abs(v) * df.ds(1)    # id 1 : bed of gllacier
 
 # objective
-J = (G - f * v ) * df.dx + j
+###J = (G - f * v ) * df.dx + j
+J = (G - f * v ) * df.dx  ### FIXME: plastic till removed for now
 F = df.derivative(J, v)
 
 # solve
-bc = df.DirichletBC(V, 0.0, 'on_boundary')
-df.solve(F == 0, v, bcs=[bc,])
+###bc = df.DirichletBC(V, 0.0, 'on_boundary')
+bc = df.DirichletBC(V, 0.0, [1,])  ### FIXME: frozen to the base for now
+
+# solver parameters to give some feedback
+sp = {
+    "snes_linesearch_type": "basic",
+    #"snes_max_it": 200,
+    #"snes_rtol": 1.0e-8,
+    #"snes_atol": 1.0e-12,
+    #"snes_stol": 0.0,
+    #"ksp_type": "preonly",
+    #"pc_type": "lu",
+    #"pc_factor_mat_solver_type": "mumps",
+    "snes_monitor": None,
+    "snes_converged_reason": None,
+}
+
+df.solve(F == 0, v, bcs=[bc,], solver_parameters=sp)
 VTKFile('result_schoof.pvd').write(v)
